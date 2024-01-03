805710c1b870e69573cf3163e66dce9f9db4dd27
IsaiasJD117 <104098161+IsaiasJD117@users.noreply.github.com>
2024-01-03 14:50:58 -0500

    added filtering, realtime updates, and pagination

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,8 @@ import CreateLink from './CreateLink';
 import Header from './Header';
 import LinkList from './LinkList';
 import Login from './Login';
-import { Route, Routes} from 'react-router-dom';
+import Search from './Search';
+import { Navigate, Route, Routes} from 'react-router-dom';
 
 const App = () => {
   return (
@@ -11,9 +12,12 @@ const App = () => {
       <Header />
       <div className="ph3 pv1 background-gray">
         <Routes>
-          <Route path="/" element={<LinkList/>} />
+          <Route path="/" element={<Navigate replace to="/new/1" />} />
           <Route path="/create" element={<CreateLink/>}/>
           <Route path="/login" element={<Login/>}/>
+          <Route path="/search" element={<Search/>} />
+          <Route path="/top" element={<LinkList/>}/>
+          <Route path="/new/:page" element={<LinkList/>}/>
         </Routes>
       </div>
     </div>
--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useMutation, gql } from '@apollo/client';
 import { useNavigate } from 'react-router-dom';
 import { FEED_QUERY } from './LinkList';
+import { AUTH_TOKEN, LINKS_PER_PAGE } from '../constants';
 
 const CREATE_LINK_MUTATION = gql`
     mutation PostMutation(
@@ -30,8 +31,16 @@ const CreateLink = () => {
       url: formState.url
     },
     update: (cache, { data: { post } }) => {
+      const take = LINKS_PER_PAGE;
+      const skip = 0;
+      const orderBy = {createdAt: 'desc'};
       const data = cache.readQuery({
         query: FEED_QUERY,
+        variables: {
+          take,
+          skip,
+          orderBy
+        }
       });
         cache.writeQuery({
           query: FEED_QUERY,
@@ -40,6 +49,11 @@ const CreateLink = () => {
               links: [post, ...data.feed.links]
             }
           },
+          variables: {
+            take,
+            skip,
+            orderBy
+          }
         });
     },
     onCompleted: () => navigate('/')
